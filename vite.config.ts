8656990d6429ef2d8f5991ae389a40dde3cff320
Alexander Krog <ak@artificialmind.ai>
2022-09-27 21:28:19 +0200

    tooling for publish on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/GcodeThingy/",
   plugins: [svelte()],
   optimizeDeps: {
     include: ["highlight.js", "highlight.js/lib/core"],
