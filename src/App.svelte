c68e4b9a46ae50250f978d752cbc6ad483a202c9
Alexander Krog <ak@artificialmind.ai>
2022-09-27 22:19:48 +0200

    make app reactive on all parameters

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
     import "carbon-components-svelte/css/g90.css";
-    import { Button, ButtonSet } from "carbon-components-svelte";
-    import Highlight from "svelte-highlight";
-    import gcode from "svelte-highlight/languages/typescript";
     import obsidian from "svelte-highlight/styles/obsidian";
-    import Fa from "svelte-fa";
-    import { faDownload, faCopy } from "@fortawesome/free-solid-svg-icons";
     import InputComponent, { MacroType } from "./lib/InputComponent.svelte";
+    import OutputComponent from "./lib/OutputComponent.svelte";
     import {
         parseGcode,
         multiply,
@@ -15,8 +11,7 @@
     } from "./lib/engine";
     import { BlockType, Routine } from "./lib/engine";
 
-    let routine: Routine | null = null;
-    $: code = routine ? routine.toString() : "";
+    export let routine: Routine | null = null;
 
     let xPitch: number = 25;
     let yPitch: number = 25;
@@ -26,57 +21,38 @@
     let afterLoopCode: string = "";
     let selectedMacro: MacroType = 1;
 
-    function fileChange(e: CustomEvent<string>) {
-        let content: string = e.detail;
+    let content: string = "";
 
-        let gcode = [content]
+    $: outputUpdate(content, xAmount, yAmount, xPitch, yPitch, beforeLoopCode, afterLoopCode);
+
+    function onChange(e: CustomEvent<string>) {
+        content = e.detail;
+    }
+
+    function outputUpdate(
+        _content: string,
+        _xAmount: number,
+        _yAmount: number,
+        _xPitch: number,
+        _yPitch: number,
+        _beforeLoopCode: string,
+        _afterLoopCode: string
+    ) {
+        if (!content) {
+            return;
+        }
+        let gcode = [_content]
             .map(parseGcode)
-            .map((gc) => multiply(gc, xAmount, yAmount, xPitch, yPitch))
+            .map((gc) => multiply(gc, _xAmount, _yAmount, _xPitch, _yPitch))
             .map((gc) =>
-                insertCustomGcodeAfter(gc, beforeLoopCode, BlockType.LoopStart)
+                insertCustomGcodeAfter(gc, _beforeLoopCode, BlockType.LoopStart)
             )
             .map((gc) =>
-                insertCustomGcodeBefore(gc, afterLoopCode, BlockType.LoopEnd)
+                insertCustomGcodeBefore(gc, _afterLoopCode, BlockType.LoopEnd)
             )[0];
 
         routine = gcode;
         console.debug("generated:", routine);
-    }
-
-    function copy(text: string) {
-        navigator.clipboard.writeText(text).then(
-            function () {
-                console.log("Async: Copying to clipboard was successful!");
-            },
-            function (err) {
-                console.error("Async: Could not copy text: ", err);
-            }
-        );
-    }
-
-    function download(filename: string, text: string) {
-        console.log("filename:", filename);
-        var element = document.createElement("a");
-        element.setAttribute(
-            "href",
-            "data:text/plain;charset=utf-8," + encodeURIComponent(text)
-        );
-        element.setAttribute("download", filename);
-
-        element.style.display = "none";
-        document.body.appendChild(element);
-
-        element.click();
-
-        document.body.removeChild(element);
-    }
-
-    function downloadGcode() {
-        if (routine.name) {
-            download(`${routine.name}.nc`, code);
-            return;
-        }
-        download("output.nc", code);
     }
 </script>
 
@@ -94,22 +70,9 @@
         bind:afterLoopCode
         bind:beforeLoopCode
         bind:selectedMacro
-        on:change={fileChange}
+        on:change={onChange}
     />
-
-    {#if code}
-        <h2 style="padding-bottom: 1.5rem;">Output</h2>
-        <ButtonSet>
-            <Button kind="primary" on:click={(e) => downloadGcode()}
-                ><Fa icon={faDownload} /> Download</Button
-            >
-            <Button kind="primary" on:click={(e) => copy(code)}
-                ><Fa icon={faCopy} /> Copy</Button
-            >
-        </ButtonSet>
-        <br />
-        <Highlight language={gcode} {code} />
-    {/if}
+    <OutputComponent bind:routine />
 </main>
 
 <style>
--- src/lib/OutputComponent.svelte
+++ src/lib/OutputComponent.svelte
@@ -1,38 +1,62 @@
 <script lang="ts">
-    import { onMount, createEventDispatcher } from "svelte";
- // import { onFileLoadFunction } from "./types";
+    import Fa from "svelte-fa";
+    import { faDownload, faCopy } from "@fortawesome/free-solid-svg-icons";
+    import { Button, ButtonSet } from "carbon-components-svelte";
+    import gcode from "svelte-highlight/languages/typescript";
+    import Highlight from "svelte-highlight";
+    import { Routine } from "./engine";
 
-    let files: FileList;
-    let fileInput: HTMLInputElement;
+    export let routine: Routine | null = null;
+    $: code = routine ? routine.toString() : "";
 
-    type onFileLoadFunction = ((a: string) => Promise<void>) | ((a: string) => void);
-
-    export let onload: onFileLoadFunction | null = null;
-
-    async function main() {
-        console.debug("mounted app");
-        fileInput.addEventListener("change", async (evt: Event) => {
-            const fileData: string = await GetFileData(files[0]);
-            console.assert(!!onload, "onload is not set");
-            if (onload) {
-                onload(fileData);
+    function copy(text: string) {
+        navigator.clipboard.writeText(text).then(
+            function () {
+                console.log("Async: Copying to clipboard was successful!");
+            },
+            function (err) {
+                console.error("Async: Could not copy text: ", err);
             }
-        });
+        );
     }
 
-    async function GetFileData(file: File): Promise<string> {
-        let p: Promise<string> = new Promise((res, _) => {
-            const reader = new FileReader();
-            reader.onload = async (e: Event) => {
-                let fr: FileReader = e.target;
-                res(fr.result as string);
-            };
-            reader.readAsText(file, "UTF-8");
-        });
-        return p;
+    function download(filename: string, text: string) {
+        console.log("filename:", filename);
+        var element = document.createElement("a");
+        element.setAttribute(
+            "href",
+            "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+        );
+        element.setAttribute("download", filename);
+
+        element.style.display = "none";
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
     }
 
-    onMount(main);
+    function downloadGcode() {
+        if (routine.name) {
+            download(`${routine.name}.nc`, code);
+            return;
+        }
+        download("output.nc", code);
+    }
+
 </script>
 
-<input bind:this={fileInput} type="file" bind:files />
+{#if code}
+    <h2 style="padding-bottom: 1.5rem;">Output</h2>
+    <ButtonSet>
+        <Button kind="primary" on:click={(e) => downloadGcode()}
+            ><Fa icon={faDownload} /> Download</Button
+        >
+        <Button kind="primary" on:click={(e) => copy(code)}
+            ><Fa icon={faCopy} /> Copy</Button
+        >
+    </ButtonSet>
+    <br />
+    <Highlight language={gcode} {code} />
+{/if}
